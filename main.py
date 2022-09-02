5f6e4fe76271dd84227916e94401d1cf50aa9a94
SebaGallegos <sebagallegos2@gmail.com>
2022-09-02 14:49:30 -0400

    Implementacion de busqueda por nombre del libro/autor

--- main.py
+++ main.py
@@ -1,24 +1,38 @@
 import csv
 
-def busquedaLibros():
-  diccionario = []
-  lista = []
+def busquedaLibros(op):
+    diccionario = []
+    lista = []
 
-  with open("lista_libros.csv", "r", newline = "") as archivo:
-    leer_archivo = csv.DictReader(archivo)
-    for fila in leer_archivo:
-      diccionario.append(fila)
+    with open("lista_libros.csv", "r", newline = "") as archivo:
+        leer_archivo = csv.DictReader(archivo)
+        for fila in leer_archivo:
+            diccionario.append(fila)
 
-  nombre = input("ingrese el nombre del libro: ")
-  for fila in diccionario:
-    if fila['nombre'].lower() == nombre.lower():
-      lista.append(fila)
-      
-  if len(lista) != 0:
-    print("-"*50)
-    for i in range(len(lista)):
-      for k,v in lista[i].items():
-        print(f"{k}: {v}")
-      print("-"*50)
+    if op.lower() == 'libro':
+        nombre = input("ingrese el nombre del libro: ")
+        for fila in diccionario:
+            if fila['nombre'].lower() == nombre.lower():
+                lista.append(fila)
 
-busquedaLibros()
+        if len(lista) != 0:
+            print("-"*50)
+            for i in range(len(lista)):
+                for k,v in lista[i].items():
+                    print(f"{k}: {v}")
+                print("-"*50)
+
+    elif op.lower() == 'autor':
+        autor = input("ingrese el nombre del autor: ")
+        for fila in diccionario:
+            if fila['autor'].lower() == autor.lower():
+                lista.append(fila)
+          
+        if len(lista) != 0:
+            print("-"*50)
+            for i in range(len(lista)):
+                for k,v in lista[i].items():
+                    print(f"{k}: {v}")
+                print("-"*50)
+
+busquedaLibros('autor')
