d0e5cf0a42005f29addc5f26be8511979d9ba1b2
SebaGallegos <sebagallegos2@gmail.com>
2022-09-07 21:17:59 -0400

    init añadido

---
+++ __init__.py
@@ -0,0 +1,13 @@
+from poo import busqueda
+
+def main():
+    bus = busqueda()
+
+    filtro = input('Bienvenido\n¿Que es lo que desea buscar? (nombre/autor): ')
+
+    if filtro.lower() == 'nombre' or filtro.lower() == 'autor':
+        print(bus.leer_archivo())
+        bus.buscar(filtro)
+
+if __name__ == '__main__':
+    main()
--- poo.py
+++ poo.py
@@ -2,26 +2,28 @@ from csv import DictReader
 
 class busqueda:
 
-    def __init__(self, filtro = 'nombre'):
-        self.filtro = filtro
+    def __init__(self):
         self.lista_libros = []
         self.resultados = []
 
     def leer_archivo(self):
-        with open("lista_libros.csv", "r", newline = "") as archivo:
+        if len(self.lista_libros) != 0:
+            self.lista_libros.clear()
+
+        with open("lista_libros.csv", "r", encoding="utf8", newline = "") as archivo:
             leer_archivo = DictReader(archivo)
             for fila in leer_archivo:
                 self.lista_libros.append(fila)
         
         return 'Archivo leido correctamente'
 
-    def buscar(self):
+    def buscar(self, filtro):
         if len(self.resultados) != 0:
             self.resultados.clear()
 
-        nombre = input(f"ingrese el {self.filtro} del libro: ")
+        nombre = input(f"ingrese el {filtro} del libro: ")
         for fila in self.lista_libros:
-            if fila[str(self.filtro)].lower() == nombre.lower():
+            if fila[str(filtro)].lower() == nombre.lower():
                 self.resultados.append(fila)
 
         if len(self.resultados) != 0:
@@ -30,8 +32,4 @@ class busqueda:
                 for k,v in self.resultados[i].items():
                     print(f"{k.capitalize()}: {v}")
                 print("-"*50)
-
-bus = busqueda('autor')
-print(bus.leer_archivo())
-bus.buscar()
 
